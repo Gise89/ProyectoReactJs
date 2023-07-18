afc1733597b311b1261e9dd4bd4ba450063146c3
Gise89 <giselasalcedo.estudio@gmail.com>
2023-07-18 20:27:39 -0300

    formulario de inscripcion

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {BrowserRouter as Router, Routes, Route, BrowserRouter} from "react-route
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import NavBar from './components/NavBar';
-
+import Cart from './components/Cart/Cart'
+import Checkout  from './components/Checkout/Checkout'
 import { CartProvider } from './context/CartContext';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path='/category/:categoryId' element={<ItemListContainer />} />
           <Route path='/item/:itemId' element={<ItemDetailContainer/>} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={Checkout />} />
           <Route path='*' element={ <h1>404 NOT FOUND</h1>} /> 
         </Routes>
       </Router>
